cc951c65c8efb4a839315ac0f989aff3d91c9b8d
KoushikVallapureddy <kvallapureddy2@gmail.com>
2025-07-30 22:29:26 +0530

    Introduction to Python

---
+++ Introduction/Comments.py
@@ -0,0 +1,19 @@
+"""
+Comments in Python are used to explain code and make it more readable.
+They are ignored by the Python interpreter and do not affect the execution of the program.
+Single-line comments start with a hash symbol (#), while multi-line comments can be enclosed in triple quotes ('''''' or """""").
+"""
+
+#Code Exmaple:
+
+#this is a single-line comment
+print('My name is Koushik')
+
+"""This is a multi-line comment using double quotations.
+It can span multiple lines and is often used to provide detailed explanations"""
+print('I am learning Python')  
+
+'''This is a multi-line comment using single quotations.
+It can also span multiple lines and is useful for longer comments or documentation'''
+print('Python is fun!')
+
---
+++ Introduction/Intro.py
@@ -0,0 +1,13 @@
+"""
+Python is one of the world's easiest and most popular programming languages.
+It is known for its simplicity and readability, making it a great choice for beginners and experienced developers
+"""
+
+"""
+Hello World in Python:
+The "Hello, World!" is a simple program that outputs the text "Hello, World!" to the screen or console.
+In Python, we use print() function to show words on the console. The words go inside quotation marks(single or doble quotation marks will work).
+"""
+
+print('Hello, World!')
+print("Hello, World!")  # Both single and double quotes work
