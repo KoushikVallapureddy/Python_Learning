d4ed33c61770ef7f3e4206fb8955ccb331561752
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-07-31 17:14:01 +0530

    Range Function

---
+++ Loops/Range_function.py
@@ -0,0 +1,30 @@
+'''
+The range() function in Python generates a sequence of numbers, 
+which is commonly used with for loops to repeat code a specific number of times.
+Create a range that starts at 0 and ends at 4:
+for i in range(5):
+    print(i)
+This will output:
+0
+1
+2
+3
+4
+You can also specify a start value:
+for i in range(2, 6):
+    print(i)
+This will output:
+2
+3
+4
+5
+And you can specify a step value (increment):
+for i in range(1, 10, 2):
+    print(i)
+This will output:
+1
+3
+5
+7
+9
+'''
