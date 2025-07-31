2be0b19df452c0f4f1ab0666bbbd26e54ea284bd
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-07-31 15:54:57 +0530

    Continue

---
+++ Loops/Continue.py
@@ -0,0 +1,18 @@
+'''
+The continue statement stops the current iteration and continues to the next iteration. 
+For example:
+for i in range(3, 9):
+    if i == 5:
+        continue
+    print(i)
+The loop will iterate through the numbers from 3 to 8, and when it reaches i=5 it will skip that iteration and continue to the next one. The output is:
+3
+4
+6
+7
+8
+
+
+
+
+'''
