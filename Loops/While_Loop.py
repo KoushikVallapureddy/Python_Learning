e03cbd4b5f709bd4f1a2e7216ef28b10793cf546
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-07-31 14:37:35 +0530

    added while loop challenge2

--- Loops/While_Loop.py
+++ Loops/While_Loop.py
@@ -25,9 +25,23 @@ Write a program that gets one input, float number.
 Use a while loop to divide the input by 2 as long as the number is bigger or equal to 3.5.
 
 Print the first number that is smaller than 3.5.
-'''
 
 var = float(input())
 while var >= 3.5:
     var /= 2
 print(var)
+
+'''
+
+#Challenge2:
+''' 
+Write a program that gets one input, float number.
+Use a while loop to divide the input by 3 as long as the number is bigger or equal to 2.5.
+Print the first number that is smaller than 2.5.
+'''
+
+number = float(input())
+while number >= 2.5:
+    number /= 3
+print(number)
+
