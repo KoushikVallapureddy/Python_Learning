7081923096d1e998015072295974d39166e5b296
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-07-31 17:07:31 +0530

    Added Challenge1 to Factorial

--- Loops/Factorial.py
+++ Loops/Factorial.py
@@ -8,3 +8,14 @@ Factorial of 6: 720 = 1 * 2 * 3 * 4 * 5 * 6
 Factorial of 2: 2 = 1 * 2
 '''
 
+#Challenge1:
+'''
+Write a program that receives one input, an integer, calculates the factorial of the input and prints it.
+For example, for input 5, the output should be 120 because 1 * 2 * 3 * 4  * 5 = 120.
+'''
+num = int(input())
+res = 1
+for i in range(1, num+1):
+    res *= i
+print(res)
+
