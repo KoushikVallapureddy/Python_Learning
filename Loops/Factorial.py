49fa965bc4dcbddd9cb0c3e27d79489ce94863dc
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-07-31 16:54:19 +0530

    Factorial

---
+++ Loops/Factorial.py
@@ -0,0 +1,10 @@
+'''
+Factorial:
+Factorial is a mathematical operation.
+Factorial of n is the product of all positive integers less than or equal to n.
+Examples,
+Factorial of 3: 6 = 1 * 2 * 3
+Factorial of 6: 720 = 1 * 2 * 3 * 4 * 5 * 6
+Factorial of 2: 2 = 1 * 2
+'''
+
