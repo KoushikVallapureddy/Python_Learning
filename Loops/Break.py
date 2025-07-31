594d3bcd6c9e252c6b07094fc8baa9a08f6d3f02
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-07-31 14:44:08 +0530

    Break

---
+++ Loops/Break.py
@@ -0,0 +1,18 @@
+'''
+Break: 
+The break statement stops the loop instantly when it's encountered.
+for example,
+for i in range(10):
+    if i == 6
+        break
+print(i)
+In the following example the loop iterates regularly until it reaches number 6. 
+Then the program enters the if statement and executes the break statement. This exits the loop immediately. The output is:
+0
+1
+2
+3
+4
+5
+
+'''
