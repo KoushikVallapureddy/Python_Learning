d5bccf33f16a4f18aa3c5d8979819018a400040c
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-07-31 16:36:28 +0530

    Added Challenge1 to Continue

--- Loops/Continue.py
+++ Loops/Continue.py
@@ -11,8 +11,16 @@ The loop will iterate through the numbers from 3 to 8, and when it reaches i=5 i
 6
 7
 8
-
-
-
-
 '''
+
+#Challenge1:
+'''
+You are given a code which prints the numbers from 1 to 20 (including).
+Your task is to add if and continue statements so that only the even numbers will be printed (2, 4, 6, ...).
+'''
+
+for i in range(1,21):
+    if i % 2 == 1:
+        continue
+    print(i)
+
