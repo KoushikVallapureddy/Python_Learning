b9f569d6fcc977241d4d41ff3e296c1e9c95e333
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-01 10:25:02 +0530

    Added Challenge2 to nested loop

--- Loops/Nested_loop.py
+++ Loops/Nested_loop.py
@@ -32,7 +32,7 @@ Use nested loops to print a rectangle with the given dimensions
 Each row should contain cols stars
 The pattern should have rows rows in total
 Example output for print_pattern(3, 4):
-'''
+
 def print_pattern(rows, cols):
     # Outer loop for rows
     for i in range(rows):
@@ -50,3 +50,37 @@ cols = int(input())
 # Call the function
 print_pattern(rows, cols)
 
+
+'''
+
+
+#Challenge2:
+
+'''
+Write a program that finds all pairs of numbers that multiply to give n using numbers from 1 to n.
+The program should show all possible combinations, including duplicate pairs in reverse order. 
+For example, both "1 6" and "6 1" should be shown, as they are considered different arrangements of the same pair.
+ Numbers can also be paired with themselves if their product equals n.
+
+For example if n = 12, the output should be:
+1 12
+2 6
+3 4
+4 3
+6 2
+12 1
+
+Because:
+1 * 12 = 12
+2 * 6 = 12
+3 * 4 = 12
+4 * 3 = 12
+6 * 2 = 12
+12 * 1 = 12
+'''
+
+n = int(input('Enter the number:'))
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i * j == n:
+            print(f'{i} {j}')
