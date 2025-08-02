5eb8d4bbc4929e69c4b567594e4155872974becd
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-02 16:43:40 +0530

    Added FizzBuzz Function

--- FizzBuzz with a Twist(Project2)/Game_overview.py
+++ FizzBuzz with a Twist(Project2)/Game_overview.py
@@ -15,3 +15,32 @@ For starters, just output the welcome message:
     Welcome to FizzBuzz!
 '''
 print('Welcome to FizzBuzz!')
+
+#Challenge2:
+'''
+Add a function named fizzbuzz that gets one number (int) as an argument, and:
+
+    If the number is divisible by 3, return "Fizz".
+    If the number is divisible by 7, return "Buzz".
+    If the number is divisible by both 3 and 7, return "FizzBuzz".
+    If none of the above conditions are met, return the number itself in a string format.
+
+After the function:
+    Get input and cast it to int
+    Call the function fizzbuzz with the input number
+    Print the output of the function
+'''
+
+def fizzbuzz(n):
+    if n % 3 == 0:
+        if n % 7 == 0:
+            return "FizzBuzz"
+        return 'Fizz'
+    elif n % 7 == 0:
+        return 'Buzz'
+    else:
+        return str(n)
+
+a = int(input())
+print(fizzbuzz(a))
+
