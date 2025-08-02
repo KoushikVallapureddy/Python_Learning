975d7455127bc1912f1ff107f2895bdab4fd81c4
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-02 15:04:30 +0530

    Recap sigma fn

---
+++ Functions/Recap_Sigma_fn.py
@@ -0,0 +1,15 @@
+'''
+Sigma Function:
+Write a function named sigma with one argument that represents a number n.
+The function will return the sum of all the numbers from 1 to n (including).
+For example, for sigma(5), the function will return 15, because 15 = 1 + 2 + 3 + 4 + 5.
+Important! You don't need to call the function we do that for you behind the scenes in a function type challenges.
+'''
+
+def sigma(n):
+    for i in range(1,n+1):
+        return sum(range(1,n+1))
+number = int(input())
+total = sigma(number)
+print(total)
+    
