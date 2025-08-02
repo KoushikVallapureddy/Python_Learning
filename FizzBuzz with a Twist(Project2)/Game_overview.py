fb7bfaa38f44468cc96f9edf461b106f07f5f10d
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-02 17:07:54 +0530

    Added Looping the numbers to the project

--- FizzBuzz with a Twist(Project2)/Game_overview.py
+++ FizzBuzz with a Twist(Project2)/Game_overview.py
@@ -31,6 +31,25 @@ After the function:
     Print the output of the function
 '''
 
+#Challenge3:
+'''
+Looping the numbers:
+Everything is ready, let's play the game!
+#Challenge:
+Loop over the numbers from 1 to the number that the user entered, 
+and each time use the function you created to calculate the FizzBuzz result and output it.
+For example, for input of 7 output:
+Welcome to FizzBuzz!
+1
+2
+Fizz
+4
+5
+Fizz
+Buzz
+
+'''
+
 def fizzbuzz(n):
     if n % 3 == 0:
         if n % 7 == 0:
@@ -42,5 +61,7 @@ def fizzbuzz(n):
         return str(n)
 
 a = int(input())
-print(fizzbuzz(a))
+
+for i in range(1, a + 1):
+    print(fizzbuzz(i))
 
