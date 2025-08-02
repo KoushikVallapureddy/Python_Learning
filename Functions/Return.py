e5b41c0f7a08e4f781ad47a3b01c4175ff74dc32
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-02 13:39:34 +0530

    Return

---
+++ Functions/Return.py
@@ -0,0 +1,12 @@
+'''
+Return Function:
+The return statement in a function is used to specify the value or values that
+the function should produce as its output.
+For example, the following function will output 100:
+    def function_name():
+        return 100
+To pass the value to a variable, write:
+    number = function_name()
+Now the number variable will hold 100 because this is what the function returned.
+'''
+
