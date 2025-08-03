0c1d53fa89a5319f0bd36aa146972b13e0d4c068
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-03 14:20:51 +0530

    Challenge2 is added to List elements

--- Lists Basics/List_elements.py
+++ Lists Basics/List_elements.py
@@ -33,3 +33,21 @@ def values(lst):
     for i in range(len(lst)):
         print(lst[i])
 
+
+#Challenge2:
+'''
+Create a function named sum_elements that receives a list as an argument and prints the sum of all the elements in the list.
+To iterate over a list, use the len() function inside the range() function:
+my_list = [10, 20, 30, 40, 50]
+    for i in range(len(my_list)):
+        # Access elements using my_list[i]
+Use this method to access each element of the list, add them together, and print the total sum.
+'''
+def sum_elements(lst):
+    total = 0
+    for i in range(len(lst)):
+        total += lst[i]
+    print(total)
+
+
+
