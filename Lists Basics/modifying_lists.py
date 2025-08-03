02a4d42521c15e085dee892a90466c2386536f6d
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-03 15:17:03 +0530

    Challenge2 is added to Modifying list

--- Lists Basics/modifying_lists.py
+++ Lists Basics/modifying_lists.py
@@ -24,15 +24,27 @@ The function will return a modified list by changing the element in an index tha
 For example with the following arguments: 
 change_element([1, 2, 3], 0, 9) the function will return [9, 2, 3]
 
-'''
 
 
 def change_element(lst, index, new_element):
     lst[index] = new_element
     return lst
 
+'''
 
+#Challenge2:
+'''
+Create a function named change_element that receives 3 arguments:
+    First argument is a list
+    Second argument is an index
+    Third argument is another list
+The function should replace the element at the given index in the first list with the first element from the second list.
 
+Example:
+change_element([1, 2, 3], 1, [5, 6, 7]) should return [1, 5, 3]
+'''
 
-
-
+def change_element(list1, index, list2):
+    if list2:
+        list1[index] = list2[0]
+    return list1
