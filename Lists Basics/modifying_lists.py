450fddf97f03fb47deeb9822839e6bfff2e92759
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-03 14:24:15 +0530

    Modifying lists

---
+++ Lists Basics/modifying_lists.py
@@ -0,0 +1,15 @@
+'''
+Modifying lists:
+In addition to accessing the elements of a list, you can also modify them. 
+To modify a specific element in a list, you can assign a new value to it using its index.
+
+Here's an example:
+my_list = ["apple", "banana", "cherry"]
+my_list[1] = "orange"
+print(my_list)
+
+Output:
+["apple", "orange", "cherry"]
+banana was changed to an orange
+'''
+
