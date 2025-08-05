330b26d435b620f6255a854ec3ef01b5554ccb94
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-05 14:27:16 +0530

    Challenge2 is added to Iterating over elements

--- Iterating Over Elements/Iterating_Over_Elements.py
+++ Iterating Over Elements/Iterating_Over_Elements.py
@@ -25,3 +25,16 @@ for word in lst:
 print(result)
 
 '''
+
+#Challenge2:
+'''
+Create a program that receives a list of numbers as input (given), and prints the sum of all even numbers in the list.
+'''
+
+numbers = input().split(',')
+total = 0
+for num in numbers:
+    if int(num.strip()) % 2 == 0:
+        total += int(num.strip())
+print(total)
+
