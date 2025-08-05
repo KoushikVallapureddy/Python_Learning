39ffc7e48d3644923ae756ce166f4b23d35adfed
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-05 21:36:29 +0530

    Iterating over strings1

---
+++ Iterating Over Elements/Iterating_over_strings1.py
@@ -0,0 +1,21 @@
+'''
+Iterating Over Strings Part 1:
+Similar to lists, you can iterate over strings
+    text = "Hey"
+    for char in text:
+        print(char)
+Output:
+    H
+    e
+    y
+Using index:
+    for i in range(len(text)):
+        print(f"position {i}: {text[i]}")
+Output:
+    position 0: H
+    position 1: e
+    position 2: y
+
+
+
+'''
