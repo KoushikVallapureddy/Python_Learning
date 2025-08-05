e8664997d22dcbace0e2690d3da98248c1298ba1
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-05 12:53:13 +0530

    Iterating Over Elements

---
+++ Iterating Over Elements/Iterating_Over_Elements.py
@@ -0,0 +1,13 @@
+'''
+Iterating Over Elements:
+Iteration means going through elements one by one in a sequence. 
+With lists, we can access each element systematically using different methods.
+The most common way to iterate through a list is using a for loop:
+    fruits = ["apple", "banana", "orange"]
+    for fruit in fruits:
+        print(fruit)
+Output:
+    apple
+    banana
+    orange
+'''
