b64e3d35fa379abecfba5fa921a7992abf881577
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-05 22:08:41 +0530

    Challenge1 is added

--- Iterating Over Elements/Iterating_over_strings1.py
+++ Iterating Over Elements/Iterating_over_strings1.py
@@ -16,6 +16,16 @@ Output:
     position 1: e
     position 2: y
 
-
-
 '''
+
+#Challenge1:
+'''
+Create a program that receives a string as input, and it prints how many times the character p (or P) is in the string.
+Some chars might be uppercase, use char.lower() to convert it to lowercase.
+'''
+text = input()
+count = 0
+for char in text:
+    if char.lower() == 'p':
+        count += 1
+print(count)
