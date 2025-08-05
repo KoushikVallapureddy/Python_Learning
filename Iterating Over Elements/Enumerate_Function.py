702e088ec636801fcd7aa4430eee0ca045d04d44
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-05 15:38:39 +0530

    Challenge2 is added

--- Iterating Over Elements/Enumerate_Function.py
+++ Iterating Over Elements/Enumerate_Function.py
@@ -31,3 +31,20 @@ print(result)
 
 '''
 
+#Challenge2:
+'''
+Write a program that receives a list of words as input (given), and prints a list of the indices of the words that are either longer than 3 characters or start with the letter 'a' (case-sensitive).
+
+To check if a string starts with some substring use: str.startswith("substring")
+'''
+
+lst = input().split()
+result = []
+for index, word in enumerate(lst):
+    if len(word.strip()) > 3 or word.startswith('a'):
+        result.append(index)
+print(result)
+
+
+
+
