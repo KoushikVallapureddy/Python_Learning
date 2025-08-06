87019471d1224a9f07882cea0c5d5a3d07fbd4c8
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-06 08:25:08 +0530

    Challenge2 is added

--- Iterating Over Elements/iterating_over_strings2.py
+++ Iterating Over Elements/iterating_over_strings2.py
@@ -46,3 +46,21 @@ result = delimiter.join(result)
 print(result)
 
 '''
+
+#Challenge2:
+'''
+Create a program that takes two inputs: a string of numbers separated by spaces, and a prefix string. 
+The program should split the number string into individual numbers, 
+add the prefix to each number, then join these modified numbers back into a single string separated by spaces. 
+Finally, print the resulting string.
+'''
+
+numbers = input()
+prefix = input()
+
+result = numbers.split()
+result = [prefix + num for num in result]
+result = ''.join(result)
+print(result)   
+
+
