b3d2ac4e5bf1dfc667cf186f0136e73919cad9de
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-07 12:03:06 +0530

    Challenge1 is added

--- List Advanced/List_Slicing1.py
+++ List Advanced/List_Slicing1.py
@@ -17,3 +17,26 @@ Omitting stop parameter:
 When stop is omitted, slice goes until the end
 '''
 
+#Challenge1:
+'''
+
+Create a program that receives a list as input (given) and prints the following sliced list (depends on the list length):
+    For odd-length lists: take the middle item and one item on each side (3 items total)
+    For even-length lists: take the two middle items
+When dividing numbers:
+    / gives you a decimal number (5/2 = 2.5)
+    // removes the decimal part (5//2 = 2)
+For this challenge, use // because list slicing only works with whole numbers.
+
+
+lst = input().split(',')
+length = len(lst)
+mid = length // 2
+if length % 2 == 0:
+    result = lst[mid-1:mid+1]
+else:
+    result = lst[mid-1:mid+2]
+print(result)
+
+'''
+
