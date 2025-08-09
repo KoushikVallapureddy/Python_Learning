95ed68ed596433abd3f3edb99485deca15373dcb
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-09 20:41:45 +0530

    Final Challenges

---
+++ Final Challenges/Flat_Asterisk_Pyramid.py
@@ -0,0 +1,13 @@
+'''
+Flat Asterisk Pyramid:
+Arithmetic operations can also be done on strings. For example:
+    str1 = "aaa" + "b"
+str1 will hold "aaab"
+
+This is the same as:
+    str1 = "aaa"
+    str1 += "b"
+Also, we can use multiplications:
+    str1 = "a"*10
+str1 will hold "aaaaaaaaaa"
+'''
