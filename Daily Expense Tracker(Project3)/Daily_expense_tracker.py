a5e135ced145ad2c335b903904d32ec2731082d5
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-09 15:36:44 +0530

    Challenge4 is added

--- Daily Expense Tracker(Project3)/Daily_expense_tracker.py
+++ Daily Expense Tracker(Project3)/Daily_expense_tracker.py
@@ -52,13 +52,68 @@ Handle the option where the user adds an expense (1).
 2. When the user selects 1 as a choice, get another input from the user, a float, and add its value to the expenses list.
 3. After adding, output:
     Expense added successfully!
-'''
+
+#Code:
 expenses = []
 while True:
     choice = input()
     if choice == '1':
         expenses.append(float(input()))
         print('Expense added successfully!')
+    elif choice == '5':
+        print('Exiting the Daily Expense Tracker. Goodbye!')
+        break
+'''
+
+#Challenge4: Veiw All Expenses
+'''
+View All Expenses:
+
+Handle the option to view all the expenses (2).
+If the expenses list is empty, output:
+    No expenses recorded yet.
+
+Otherwise, output the list in the following format:
+    Your expenses:
+    1. 23.1
+    2. 35.5
+    3. 99.99
+    4. 15.2
+Assuming the expenses entered before were 23.1, 35.5, 99.99 and 15.2. (In that order!)
+
+
+#Code:
+expenses = []
+while True:
+    choice = input()
+    if choice == '1':
+        expenses.append(float(input()))
+        print('Expense added successfully!')
+    elif choice == '2':
+        if not expenses:
+            print('No expenses recorded yet.')
+        else:
+            print('Your expenses:')
+            for i in range(len(expenses)):
+                print(f'{i + 1}. {expenses[i]}')
+    elif choice == '5':
+        print('Exiting the Daily Expense Tracker. Goodbye!')
+        break
+'''
+
+expenses = []
+while True:
+    choice = input()
+    if choice == '1':
+        expenses.append(float(input()))
+        print('Expense added successfully!')
+    elif choice == '2':
+        if not expenses:
+            print('No expenses recorded yet.')
+        else:
+            print('Your expenses:')
+            for i in range(len(expenses)):
+                print(f'{i + 1}. {expenses[i]}')
     elif choice == '5':
         print('Exiting the Daily Expense Tracker. Goodbye!')
         break
