ce023e93b7f29995352d01671884424e04eaf2c3
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-09 13:09:29 +0530

    Challenge1 is added

--- List Advanced/Membership.py
+++ List Advanced/Membership.py
@@ -5,3 +5,15 @@ You can check whether an element is in the a list or not (in, not in):
     3 not in [1, 2]  # True
     3 in [1, 2]      # False
 '''
+
+#Challenege1:
+'''
+Create a program that receives two lists and prints a new list of all elements that are in the first list but NOT in the second list.
+'''
+lst1 = input().split(',')
+lst2 = input().split(',')
+result = []
+for element in lst1:
+    if element not in lst2:
+        result.append(element)
+print(result)
