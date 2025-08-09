71593d48b87e2c4b46a2b66ff4e8b7a1c9bceccf
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-09 14:16:35 +0530

    Project3 is added

---
+++ Daily Expense Tracker(Project3)/Daily_expense_tracker.py
@@ -0,0 +1,5 @@
+'''
+You will build a simple program to manage daily expenses.
+The user can add, view, and clear expenses, as well as calculate the total and average.
+This project reinforces basic programming concepts like lists, loops, and functions in a practical, real-life scenario.
+'''
