3c738642812544e6446f9301349acc449547b0ce
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-10 11:48:21 +0530

    Placeholder Variables

--- Section2 - Logic & Flow/Variable Exploration/4. Placeholder_variables.py
+++ Section2 - Logic & Flow/Variable Exploration/4. Placeholder_variables.py
@@ -27,3 +27,17 @@ Here, _ is used to ignore the second, third, and fourth elements of the tuple.
 
 '''
 
+#Challenge:
+'''
+Write a Python program that demonstrates the use of placeholder variables in different scenarios:
+    Create a loop that iterates 5 times. Use a placeholder variable (an underscore) since the loop variable is not used within the loop. In each iteration, print the word Iteration.
+    You have a list of numbers: [10, 20, 30, 40, 50]. Unpack this list into three variables: first, middle, and last. Use a placeholder variable to ignore the second and fourth values. Then, print the values of first, middle, and last.
+Check the test case for the output format!
+'''
+for _ in range(5):
+    print("Iteration")
+numbers = [10, 20, 30, 40 ,50]
+first, _, middle, _, last = numbers
+print(f'First: {first}')
+print(f'Middle: {middle}')
+print(f'Last: {last}')
