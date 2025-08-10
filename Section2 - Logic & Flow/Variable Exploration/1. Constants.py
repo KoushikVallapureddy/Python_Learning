8424534ee23e13b6d06a66219e8271d90227f7df
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-10 10:54:32 +0530

    Section is added. and Variable Exploration is added. Constants is added

---
+++ Section2 - Logic & Flow/Variable Exploration/1. Constants.py
@@ -0,0 +1,19 @@
+'''
+Constants:
+In programming, a constant is a type of variable whose value cannot be changed during the execution of a program. 
+Constants are useful for defining values that are used multiple times throughout a program, 
+such as mathematical constants like PI or the number of hours in a day.
+
+In Python, there is no strict enforcement of constants like in some other languages (such as c++). 
+However, there is a widely adopted convention to indicate that a variable should be treated as a constant. 
+This is done by writing the variable name in all uppercase letters, often with underscores to separate words. 
+
+For example:
+    PI = 3.14159
+    HOURS_IN_A_DAY = 24
+    MAX_USERS = 100
+
+When you see variables named this way in Python code, it's a signal that these values should not be altered. 
+This convention helps make the code more readable and maintainable. 
+It's important to note that this is just a naming convention; the Python interpreter itself does not prevent you from changing the value of these variables.
+'''
