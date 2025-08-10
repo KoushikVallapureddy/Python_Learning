e1c595b777519b9fe465b448415be3d85a7f0640
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-10 15:17:24 +0530

    Challenge2 is added

--- Section2 - Logic & Flow/2. Dictionaries P1/3. Modifying_ Dictionaries.py
+++ Section2 - Logic & Flow/2. Dictionaries P1/3. Modifying_ Dictionaries.py
@@ -29,12 +29,12 @@ In these examples, we first add a new key-value pair to an empty dictionary.
 Then, we update the value of an existing key. Finally, we delete a key-value pair using the del keyword.
 '''
 
-#Challenge:
+#Challenge1:
 '''
 Create a function named update_employee_info that takes three parameters: employee_dict (a dictionary), key (a string), and value. 
 The function should update the employee_dict with the new key and value. If the key already exists, its value should be updated. 
 If the key does not exist, a new key-value pair should be added. The function should return the updated dictionary.
-''' 
+ 
 
 def update_employee_info(employee_dict, key, value):
     employee_dict[key] = value
@@ -49,3 +49,31 @@ employee_dict = {
 key = input()
 value = input()
 print(update_employee_info(employee_dict, key, value))
+
+'''
+
+#Challenge2:
+'''
+Create a function named update_inventory that takes three parameters: inventory_dict (a dictionary), item (a string), and quantity (an integer). 
+The function should update the inventory_dict with the new item and quantity. 
+    If the item already exists in the inventory, its quantity should be increased by the given amount. 
+If the item does not exist, a new item should be added with the given quantity. 
+The function should return the updated dictionary.
+'''
+
+
+def update_inventory(inventory_dict, item, quantity):
+    if item in inventory_dict:
+        inventory_dict[item] += quantity
+    else:
+        inventory_dict[item] = quantity
+    return inventory_dict
+inventory_dict = {
+    'apples' : 10,
+    "bananas" : 15,
+    "oranges" : 12
+}
+
+item = input()
+quantity = int(input())
+print(update_inventory(inventory_dict, item, quantity))
