f82de53d26c4b804709a8f9177b9c5b510c2cd87
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-10 14:23:33 +0530

    Aceessing values in Dictionary

---
+++ Section2 - Logic & Flow/2. Dictionaries P1/2. Accessing_values.py
@@ -0,0 +1,25 @@
+'''
+Acessing Values:
+In a dictionary, each key is associated with a value. To access a value, you use its key. 
+This is similar to how you would look up a word in a physical dictionary to find its definition.
+
+Here's how you can access values in a Python dictionary:
+
+# Dictionary of country capitals
+    country_capitals = {
+    "USA": "Washington, D.C.",
+    "France": "Paris",
+    "Japan": "Tokyo"
+    }
+
+# Accessing values using keys
+    print(country_capitals["USA"])
+    print(country_capitals["France"])
+
+# Accessing a value that does not exist
+# print(country_capitals["Germany"])  # This will cause an error
+
+In this example, we access the capital of the USA by using the key "USA". 
+If you try to access a key that does not exist in the dictionary, Python will raise a KeyError.
+
+'''
