8e561e0a8fcd828fe30a609f501f31b71d230f5c
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-10 11:59:33 +0530

    Challenge1 is added

--- Section2 - Logic & Flow/Variable Exploration/5. Round_Numbers.py
+++ Section2 - Logic & Flow/Variable Exploration/5. Round_Numbers.py
@@ -10,8 +10,23 @@ Examples:
     print(round(4.567))     # 5
     print(round(4.567, 2))  # 4.57
     print(round(456.78, -1))  # 460
-    
+
 Python rounds halfway cases to the nearest even number:
     print(round(2.5))  # 2
     print(round(3.5))  # 4
+'''
+
+#Challenge1:
+'''
+Write a program that:
+    Takes a number as input from the user (float).
+    Takes the number of decimal places to round to (integer).
+    Outputs the rounded number.
+
+    
+number = float(input())
+integer = int(input())
+rounded_number = round(number, integer)
+print(rounded_number)
+
 '''
