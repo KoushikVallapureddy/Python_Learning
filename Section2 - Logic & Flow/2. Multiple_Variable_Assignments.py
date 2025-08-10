d2ae868264e69353595770b8fbfea90d493795d0
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-10 11:13:57 +0530

    Multiple Variable Assignment

---
+++ Section2 - Logic & Flow/2. Multiple_Variable_Assignments.py
@@ -0,0 +1,25 @@
+'''
+Multiple Variable Assignments:
+
+In Python, you can assign values to multiple variables in a single line. 
+This feature can make your code more concise and readable. Let's explore how to use multiple variable assignments effectively.
+
+Basic Multiple Assignments:
+    a, b, c = 1, 2, 3
+    print(a)  # Output: 1
+    print(b)  # Output: 2
+    print(c)  # Output: 3
+
+Assigning the same value to multiple variables:
+    x = y = z = 10
+    print(x)  # Output: 10
+    print(y)  # Output: 10
+    print(z)  # Output: 10
+
+Assigning values from a list:
+    numbers = [4, 5, 6]
+    a, b, c = numbers
+    print(a)  # Output: 4
+    print(b)  # Output: 5
+    print(c)  # Output: 6
+'''
--- Section2 - Logic & Flow/Variable Exploration/1. Constants.py
+++ Section2 - Logic & Flow/Variable Exploration/1. Constants.py
@@ -17,3 +17,18 @@ When you see variables named this way in Python code, it's a signal that these v
 This convention helps make the code more readable and maintainable. 
 It's important to note that this is just a naming convention; the Python interpreter itself does not prevent you from changing the value of these variables.
 '''
+
+#Challeneg1:
+'''
+Create a Python program that calculates the area of a circle using a constant for the value of PI. 
+Define a constant named PI and set its value to 3.14159. Then, prompt the user to enter the radius of the circle. 
+Calculate the area of the circle using the formula (area = PI * radius**2) and print the result.
+
+Remember to follow the naming convention for constants by using all uppercase letters for the variable PI.
+
+'''
+
+PI = 3.14159
+radius = float(input())
+area = PI * radius ** 2
+print(f'{area}')
