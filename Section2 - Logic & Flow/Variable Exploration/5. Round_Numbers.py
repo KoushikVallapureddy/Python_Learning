10b9c18758cb1e71387b1f4c8e5a470461c29d32
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-10 12:11:06 +0530

    Challenge2 is added

--- Section2 - Logic & Flow/Variable Exploration/5. Round_Numbers.py
+++ Section2 - Logic & Flow/Variable Exploration/5. Round_Numbers.py
@@ -23,10 +23,40 @@ Write a program that:
     Takes the number of decimal places to round to (integer).
     Outputs the rounded number.
 
-    
+
 number = float(input())
 integer = int(input())
 rounded_number = round(number, integer)
 print(rounded_number)
 
 '''
+
+#Challenge2:
+'''
+Create a function named calculate_discount that takes two parameters:
+    price: The original price of an item (float)
+    discount_percentage: The discount percentage (float)
+
+The function should:
+    Calculate the discount amount
+    Subtract the discount amount from the original price
+    Round the result to 2 decimal places
+    Return the final discounted price
+
+For example, if the original price is $100 and the discount is 20%, the function should return $80.00.
+'''
+
+def calculate_discount(price, discount_percentage):
+    discount_amount = price * discount_percentage /100
+    final_amount = price - discount_amount
+    final_discounted_price = round(final_amount, 2)
+    return final_discounted_price
+
+price = float(input())
+discount_percentage = float(input())
+calculate_discount(price, discount_percentage)
+print(calculate_discount(price, discount_percentage))
+
+
+
+
