2a355d15585576f8ea0ddb41fd49092ab4601315
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-11 12:01:10 +0530

    Challenge2 is added.

--- Section2 - Logic & Flow/3. Dictionaries P2/3. Checking_for_Keys.py
+++ Section2 - Logic & Flow/3. Dictionaries P2/3. Checking_for_Keys.py
@@ -36,7 +36,6 @@ Check if "Alice" is a key in the dictionary.
 Check if "John" is not a key in the dictionary.
     If it does not exist, print: 
     "John is not in the company."
-'''
 
 employee_data = {'Alice':'HR', 'Bob':'Engineering', 'Diana':'Marketing'}
 # Check if "Alice" is a key in the dictionary
@@ -45,4 +44,31 @@ if 'Alice' in employee_data:
 #Check if "John" is not a key in the dictionary
 if 'John' not in employee_data:
     print('John is not in the company.')
+
+'''
+
+#Challenge2:
+'''
+Create a function named check_inventory that takes two parameters: inventory (a dictionary where keys are item names and values are quantities) and item (a
+string representing the item to check). 
+The function should:
+    Check if the item is in the inventory.
+    If the item is in stock, return the string: "<item> is in stock. Quantity: <quantity>".
+    If the item is not in stock, return the string: "<item> is not in stock."
+'''
+
+def check_inventory(inventory, item):
+    if item in inventory:
+        return f'{item} is in stock. Quantity: {inventory[item]}'
+    else:
+        return f'{item} is not in stock.'
+inventory ={
+    'apples': 10,
+    'bananas': 5,
+    'oranges': 0
+}
+
+item = input()
+print(check_inventory(inventory, item))
+
 
