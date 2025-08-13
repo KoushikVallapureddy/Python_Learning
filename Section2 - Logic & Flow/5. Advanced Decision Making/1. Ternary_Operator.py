285bea9a04a349b5a9d58575cbf75458fcf7cf48
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-13 12:09:06 +0530

    Added Challenge

--- Section2 - Logic & Flow/5. Advanced Decision Making/1. Ternary_Operator.py
+++ Section2 - Logic & Flow/5. Advanced Decision Making/1. Ternary_Operator.py
@@ -18,3 +18,15 @@ In this example, if age is greater than or equal to 18, status will be assigned 
 Otherwise, it will be assigned "Not Eligible".
 The ternary operator is particularly useful for simple conditional assignments where you want to choose between two values based on a condition.
 '''
+
+
+#Challenge:
+'''
+Create a program that takes an integer score from the user. The program should use the ternary operator to determine the status based on the score:
+    If the score is greater than or equal to 50, the status should be "Pass".
+    If the score is less than 50, the status should be "Fail".
+The program should output the determined status.
+'''
+score = int(input())
+result = 'Pass' if score >= 50 else 'Fail'
+print(result)
