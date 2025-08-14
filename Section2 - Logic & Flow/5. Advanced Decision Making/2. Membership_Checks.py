9ece53f4202b1b4107b0c88cefde54fbe14b3478
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-14 09:58:33 +0530

    Challenge is added

--- Section2 - Logic & Flow/5. Advanced Decision Making/2. Membership_Checks.py
+++ Section2 - Logic & Flow/5. Advanced Decision Making/2. Membership_Checks.py
@@ -16,3 +16,29 @@ For dictionaries, membership checks apply to keys by default:
     print("name" in my_dict)  # True
     print("Alice" in my_dict)  # False
 '''
+
+#Challenge
+
+'''
+You are given a list of names and a dictionary of student grades. Write a program that:
+    Checks if the name "Alice" is in the list.
+    Checks if the name "David" is not in the list.
+    Checks if the key "Bob" exists in the dictionary.
+    Checks if the key "Eve" does not exist in the dictionary.
+Check the test case for the output format!
+'''
+
+name = ['Alice', 'Bob', 'Charlie']
+grades = {'Alice': 85, 'Bob': 90, 'Charlie': 78}
+
+result = 'Alice is in the list.' if 'Alice' in name else 'Alice is not in the list.'
+print(result)
+
+result = 'David is in the list.' if "David" in name else "David is not in the list."
+print(result)
+
+result = 'Bob is in the dictionary.' if 'Bob' in grades else "Bob is not in the dictionary."
+print(result)
+
+result = "Eve is in the dictionary." if "Eve" in grades else "Eve is not in the dictionary."
+print(result)
