6928c6b9b718e11a59c73709af1fe1d267198dce
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-15 16:16:44 +0530

    Challenge is added

--- Section2 - Logic & Flow/6. Sets P1/3. Set_methods.py
+++ Section2 - Logic & Flow/6. Sets P1/3. Set_methods.py
@@ -22,3 +22,26 @@ clear(): Removes all elements from the set, making it empty.
     print(my_set)
     # Output: set()
 '''
+
+#Challenge:
+'''
+Write a program that performs the following tasks:
+
+1. Create a set called numbers containing the values 10, 20, 30, 40, 50.
+2. Use the discard() method to remove 35 from the set.
+3. Use the pop() method to remove an arbitrary element and store it in a variable called popped_element.
+4. Use the clear() method to empty the set.
+5. Print the following:
+    The set after using discard().
+    The value of popped_element.
+    The set after using clear().
+'''
+
+numbers = {10, 20, 30, 40, 50}
+numbers.discard(35)
+print(numbers)
+popped_element = numbers.pop()
+print(popped_element)
+numbers.clear()
+print(numbers)
+
