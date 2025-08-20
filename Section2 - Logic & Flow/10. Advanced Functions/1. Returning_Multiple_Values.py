2a3b980108d7daf6b85ed1b328d322c0143b9e77
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-20 15:16:55 +0530

    Challenge2 is added

--- Section2 - Logic & Flow/10. Advanced Functions/1. Returning_Multiple_Values.py
+++ Section2 - Logic & Flow/10. Advanced Functions/1. Returning_Multiple_Values.py
@@ -20,7 +20,7 @@ Returning multiple values is useful when you need to return more than one piece 
 Instead of creating a complex data structure like a list or a dictionary, you can simply return the values as a tuple.
 '''
 
-#Challenge:
+#Challenge1:
 '''
 Create a function named get_student_info that takes no arguments. The function should return a tuple containing the following information about a student:
     Name: "Bob"
@@ -46,3 +46,23 @@ print(student_name)
 print(student_age)
 print(student_major)
 
+
+
+#Challenge2:
+
+'''
+Create a function named get_product_info that takes no arguments. 
+The function should return a tuple containing the following information about a product:
+    Name: "Laptop"
+    Price: 999.99
+    Rating: 4.5
+After defining the function, call it and unpack the returned values into three variables: product_name, product_price, and product_rating. 
+Then, print the values of these variables.
+'''
+
+def get_product_info():
+    return 'Laptop', 999.99, 4.5
+product_name, product_price, product_rating = get_product_info()
+print(product_name)
+print(product_price)
+print(product_rating)
