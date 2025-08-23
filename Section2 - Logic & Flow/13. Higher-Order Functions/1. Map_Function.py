f529105289cdaed4e3168086dbd97debcc9c090b
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-23 18:08:22 +0530

    Map Functions

---
+++ Section2 - Logic & Flow/13. Higher-Order Functions/1. Map_Function.py
@@ -0,0 +1,35 @@
+'''
+The Map Function:
+The map() function takes two main things:
+    A function (which contains instructions for what to do)
+    A sequence of items (like a list, or any collection of elements)
+
+It then applies that function to each item in your sequence, one by one, and gives you back all the results.
+
+For example:
+    def square(n):
+        return n * n
+    numbers = [1, 2, 3, 4, 5]  # This is just a list of numbers
+    squared_numbers = map(square, numbers)
+    print(list(squared_numbers))
+    # Output: [1, 4, 9, 16, 25]
+
+What happened here:
+    We have a list of numbers: [1, 2, 3, 4, 5]
+    The map() function takes each number, one at a time
+    It puts each number through our square function
+    It collects all the results in order
+
+You can think of it like an assembly line:  
+    Number 1 goes in → square function makes it 1
+    Number 2 goes in → square function makes it 4
+    Number 3 goes in → square function makes it 9 And so on...
+
+You can also use a lambda function instead of defining a separate function:
+    numbers = [1, 2, 3, 4, 5]
+    squared_numbers = map(lambda n: n * n, numbers)
+    print(list(squared_numbers))
+    # Output: [1, 4, 9, 16, 25]
+    
+The map() function works with any collection of items - not just numbers. It could be strings, or any other type of data you want to process in the same way.
+'''
