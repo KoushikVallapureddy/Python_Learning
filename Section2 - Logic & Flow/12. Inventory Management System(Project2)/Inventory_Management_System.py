6137511e4b1bd1337f34d6708aaf7b6f35f9cdb7
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-23 07:52:39 +0530

    Update Stock code is added

--- Section2 - Logic & Flow/12. Inventory Management System(Project2)/Inventory_Management_System.py
+++ Section2 - Logic & Flow/12. Inventory Management System(Project2)/Inventory_Management_System.py
@@ -32,7 +32,29 @@ Add (replace) the following block of code at the bottom of your code:
     print(inventory)
 '''
 
- #Challege1 Code:
+#Challenge3: Update Stock
+'''
+Create a function named update_stock that takes two arguments: item (string) and quantity (int). The function should:
+
+    Check if the item exists in the inventory dictionary.
+        If it does not exist, print "Error: Item '<item>' not found.".
+    If the item exists, calculate the new stock by adding the quantity to the current stock.
+        If the new stock is negative, print "Error: Insufficient stock for '<item>'." and do not update.
+        Otherwise, update the stock in the inventory.
+    Print "Stock for '<item>' updated successfully.".
+
+Add (replace) the following block of code at the bottom of your code:
+    add_item("Apple", 0.5, 100)
+    add_item("Banana", 0.2, 50)
+    add_item("Apple", 0.6, 30)  # Should print an error
+    update_stock("Apple", -20)
+    update_stock("Banana", 30)
+    update_stock("Orange", 10)  # Should print an error
+    update_stock("Apple", -90)
+    print(inventory)  
+
+'''
+#Challege1 Code:
 inventory = {}
 
 #Challenge2 Code: Add item
@@ -43,7 +65,32 @@ def add_item(item, price, stock):
         inventory[item] = {"price": float(price), "stock": int(stock)}
         print(f"Item '{item}' added successfully.")
 
+#Below code is replaced and moved to Challenge3 code. In order to test Challenge2, uncomment below code and comment other codes.
+'''
 add_item("Apple", 0.5, 100)
 add_item("Banana", 0.2, 50)
 add_item("Apple", 0.6, 30)  # Should print an error
 print(inventory)
+'''
+#Challenge3 Code: Update Stock
+
+def update_stock(item, quantity):
+    if item not in inventory:
+        print(f"Error: Item '{item}' not found.")
+        return
+    new_stock = inventory[item]["stock"] + quantity
+    if new_stock < 0:
+        print(f"Error: Insufficient stock for '{item}'.")
+        return
+    inventory[item]["stock"] = new_stock
+    print(f"Stock for '{item}' updated successfully.")
+
+add_item("Apple", 0.5, 100)
+add_item("Banana", 0.2, 50)
+add_item("Apple", 0.6, 30)  # Should print an error
+update_stock("Apple", -20)
+update_stock("Banana", 30)
+update_stock("Orange", 10)  # Should print an error
+update_stock("Apple", -90)
+print(inventory)
+        
