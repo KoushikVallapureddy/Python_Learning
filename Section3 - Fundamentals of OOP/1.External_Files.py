e2c74baa95b03bd5724618850c9584cf2ddd5f7e
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-25 11:40:56 +0530

    Challenge

--- Section3 - Fundamentals of OOP/1.External_Files.py
+++ Section3 - Fundamentals of OOP/1.External_Files.py
@@ -20,7 +20,13 @@ Create an instance and use it
 
 Output:
     Hello, I'm Alice
-    
+
 The from my_class import MyClass statement connects the my_class.py file to your program. 
 The first my_class is the filename (without .py), and MyClass is the class name inside that file.
 '''
+
+#Challenge:
+'''
+You are given Python files (my_class.py and driver.py), add an import statement in the driver.py file to connect the files and use the class from the external file!
+'''
+
