90b5de3f41ec7bb6654bec84c392abd16c975f95
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-29 22:14:55 +0530

    Renamed

--- Section3 - Fundamentals of OOP/1. External files/my_class.py
+++
@@ -1,8 +0,0 @@
-
-#Challenge:
-'''
-You are given Python files (my_class.py and driver.py), add an import statement in the driver.py file to connect the files and use the class from the external file!
-'''
-class MyClass:
-    def __init__(self):
-        pass
