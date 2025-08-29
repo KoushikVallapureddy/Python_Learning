ca25c72515d64922d831310c2924786b67220b32
Koushik Reddy Vallapureddy <kvallapureddy2@gmail.com>
2025-08-29 19:03:35 +0530

    The self Parameter

---
+++ Section3 - Fundamentals of OOP/4. The self Parameter/driver.py
@@ -0,0 +1,11 @@
+#The Self Parameter:
+
+from car import Car
+
+# Test your code
+my_car = Car()
+my_car.year = 2020
+my_car.make = "Toyota"
+my_car.model = "Corolla"
+
+my_car.display_info()  # Should print: This car is a 2020 Toyota Corolla
